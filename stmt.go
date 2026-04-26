d5820c4284de982a65629d72e5a7267176ee370a
mdmourao <martimmourao@gmail.com>
2026-04-26 18:39:01 +0100

    feat(god1): intercept sqlite_version() probe and return configured version
    * Added import for `strings`
    * Defined a constant `d1ReportedSQLiteVersion` with value `"3.53.0"`
    * Implemented logic to intercept `sqlite_version()` queries and return the configured version
      * Added debug logging for intercepted probes
      * Created a new `Rows` instance with the reported SQLite version data

--- stmt.go
+++ stmt.go
@@ -3,7 +3,11 @@ package god1
 import (
 	"context"
 	"database/sql/driver"
+	"strings"
 )
+
+// TODO - make this configurable?
+const d1ReportedSQLiteVersion = "3.53.0"
 
 // https://pkg.go.dev/database/sql/driver#Stmt
 // driver.Stmt
@@ -32,6 +36,17 @@ func (s *Stmt) Exec(args []driver.Value) (driver.Result, error) {
 // TODO Deprecated
 func (s *Stmt) Query(args []driver.Value) (driver.Rows, error) {
 	s.conn.logger.Debug("Query called", "query", s.query, "args", args)
+
+	// TODO - best way to detect this?
+	if strings.Contains(s.query, "sqlite_version()") {
+		s.conn.logger.Debug("Intercepted sqlite_version() probe", "version", d1ReportedSQLiteVersion)
+		return &Rows{
+			columns: []string{"sqlite_version()"},
+			data: []map[string]any{
+				{"sqlite_version()": d1ReportedSQLiteVersion},
+			},
+		}, nil
+	}
 
 	// TODO - review parsing
 	params := make([]any, len(args))
